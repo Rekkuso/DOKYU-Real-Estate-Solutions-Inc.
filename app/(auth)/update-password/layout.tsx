5b7979848862ca1657a830c54342eb8fdf5d5dac
Rekkuso <rexguimespinosa060403@gmail.com>
2026-07-22 14:05:33 +0800

    fix: protect /update-password route

---
+++ app/(auth)/update-password/layout.tsx
@@ -0,0 +1,22 @@
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function UpdatePasswordLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  return <>{children}</>;
+}
